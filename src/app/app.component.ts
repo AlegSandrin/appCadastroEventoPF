64b02e3e90abbd8580e1a3436e4f93fec56c5279
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-02-18 02:58:22 -0300

    implementado verificação de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { getAuth, signOut } from "firebase/auth";
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  usuario: any;
+
+  constructor(private router: Router,
+    private auth: AngularFireAuth
+    ) {}
+
+  async ngOnInit(){
+    
+    this.auth.onAuthStateChanged(async user => { // verifica se o usuário esta logado
+      if (user) {
+    this.router.navigateByUrl("/perfil");  // se estiver logado manda pra página inicial  
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const uid = user.uid;
+    const db = getFirestore();
+    const docRef = doc(db, "users", uid);
+
+
+    try {
+      const doc = await getDoc(docRef);
+      this.usuario = doc.data();
+    } catch (e) {
+      console.log("Error getting cached document:", e);
+    }
+      }
+      else{
+        this.router.navigateByUrl("/home");
+      }
+    });
+
+  }
 
 signOut(){
 
